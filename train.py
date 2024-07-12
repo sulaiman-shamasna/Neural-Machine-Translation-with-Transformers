f7151f4a85272b3c74412ad7fc47e804aa48be89
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-07-12 21:08:26 +0200

    chore: :rocket: Add training loop

---
+++ train.py
@@ -0,0 +1,99 @@
+from prepare_data import *
+from models import *
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+num_layers = 4
+d_model = 128
+dff = 512
+num_heads = 8
+dropout_rate = 0.1
+
+
+transformer = Transformer(
+    num_layers=num_layers,
+    d_model=d_model,
+    num_heads=num_heads,
+    dff=dff,
+    input_vocab_size=tokenizers.pt.get_vocab_size().numpy(),
+    target_vocab_size=tokenizers.en.get_vocab_size().numpy(),
+    dropout_rate=dropout_rate)
+
+output = transformer((pt, en))
+
+print(en.shape)
+print(pt.shape)
+print(output.shape)
+
+
+
+attn_scores = transformer.decoder.dec_layers[-1].last_attn_scores
+print(attn_scores.shape)  # (batch, heads, target_seq, input_seq)
+
+transformer.summary()
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=4000):
+    super().__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    step = tf.cast(step, dtype=tf.float32)
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+learning_rate = CustomSchedule(d_model)
+
+optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                     epsilon=1e-9)
+
+plt.plot(learning_rate(tf.range(40000, dtype=tf.float32)))
+plt.ylabel('Learning Rate')
+plt.xlabel('Train Step')
+
+
+def masked_loss(label, pred):
+  mask = label != 0
+  loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+    from_logits=True, reduction='none')
+  loss = loss_object(label, pred)
+
+  mask = tf.cast(mask, dtype=loss.dtype)
+  loss *= mask
+
+  loss = tf.reduce_sum(loss)/tf.reduce_sum(mask)
+  return loss
+
+
+def masked_accuracy(label, pred):
+  pred = tf.argmax(pred, axis=2)
+  label = tf.cast(label, pred.dtype)
+  match = label == pred
+
+  mask = label != 0
+
+  match = match & mask
+
+  match = tf.cast(match, dtype=tf.float32)
+  mask = tf.cast(mask, dtype=tf.float32)
+  return tf.reduce_sum(match)/tf.reduce_sum(mask)
+
+transformer.compile(
+    loss=masked_loss,
+    optimizer=optimizer,
+    metrics=[masked_accuracy])
+
+
+transformer.fit(train_batches,
+                epochs=3,
+                validation_data=val_batches)
+
+transformer.save('saver_model')
