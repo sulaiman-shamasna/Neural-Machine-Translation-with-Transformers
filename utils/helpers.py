c312e7d6199d5a898d89969f161695aaa596080f
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-07-16 19:51:53 +0200

    chore: :construction_worker: Add some helper functions

---
+++ utils/helpers.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def print_translation(sentence, tokens, ground_truth):
+  print(f'{"Input:":15s}: {sentence}')
+  print(f'{"Prediction":15s}: {tokens.numpy().decode("utf-8")}')
+  print(f'{"Ground truth":15s}: {ground_truth}')
+
+
+def plot_attention_head(in_tokens, translated_tokens, attention):
+  # The model didn't generate `<START>` in the output. Skip it.
+  translated_tokens = translated_tokens[1:]
+
+  ax = plt.gca()
+  ax.matshow(attention)
+  ax.set_xticks(range(len(in_tokens)))
+  ax.set_yticks(range(len(translated_tokens)))
+
+  labels = [label.decode('utf-8') for label in in_tokens.numpy()]
+  ax.set_xticklabels(
+      labels, rotation=90)
+
+  labels = [label.decode('utf-8') for label in translated_tokens.numpy()]
+  ax.set_yticklabels(labels)
